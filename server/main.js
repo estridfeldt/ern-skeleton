ce6c1b0fecd4a027acb0beec1eb3a1275c04bc4e
emst <emil.stridfeldt@gmail.com>
2017-03-07 21:05:23 +0100

    skeleton project

--- server/main.js
+++ server/main.js
@@ -4,19 +4,15 @@ const Express = require('express')
 
 const app = new Express();
 const server = new Server(app);
-//app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, 'views'));
 
-// define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, '../dist')));
-
 
 
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
 server.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  console.info(`Server running on http://localhost:${port} [${env}]`);
+    if (err) {
+        return console.error(err);
+    }
+    console.info(`Server running on http://localhost:${port} [${env}]`);
 });
